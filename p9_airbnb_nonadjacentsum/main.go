b65ab5615ce4fb7bb8c3c56663095b4159aa1226
Nguyen Nhat Cuong <nguyen.nhat.cuong@gmail.com>
2018-09-16 19:14:23 +0200

    p9 optimization to make it linear

--- p9_airbnb_nonadjacentsum/main.go
+++ p9_airbnb_nonadjacentsum/main.go
@@ -1,29 +1,40 @@
+// This problem was asked by Airbnb.
+// Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
+// For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+
 package main
 
 import "fmt"
 
-func MaxNonAdjacentSum(inputArray []int) int {
-	switch l := len(inputArray); l {
+func MaxNonAdjacentSum(inputArray []int, fromIndex int, mapTempResult map[int]int) int {
+	if val, ok := mapTempResult[fromIndex]; ok {
+		return val
+	}
+	switch l := len(inputArray) - fromIndex; l {
 	case 0:
+		mapTempResult[fromIndex] = 0
 		return 0
 	case 1:
-		return inputArray[0]
+		mapTempResult[fromIndex] = inputArray[fromIndex]
+		return inputArray[fromIndex]
 	default:
 		maxWithFirstElt := 0
-		if inputArray[0] > 0 {
-			maxWithFirstElt = inputArray[0]
+		if firstElt := inputArray[fromIndex]; firstElt > 0 {
+			maxWithFirstElt = firstElt
 		}
-		maxWithFirstElt = maxWithFirstElt + MaxNonAdjacentSum(inputArray[2:])
+		maxWithFirstElt = maxWithFirstElt + MaxNonAdjacentSum(inputArray, fromIndex+2, mapTempResult)
 
-		maxWithoutFirstElt := MaxNonAdjacentSum(inputArray[1:])
+		maxWithoutFirstElt := MaxNonAdjacentSum(inputArray, fromIndex+1, mapTempResult)
 
 		if maxWithFirstElt > maxWithoutFirstElt {
+			mapTempResult[fromIndex] = maxWithFirstElt
 			return maxWithFirstElt
 		}
+		mapTempResult[fromIndex] = maxWithoutFirstElt
 		return maxWithoutFirstElt
 	}
 }
 
 func main() {
-	fmt.Println(MaxNonAdjacentSum([]int{1, -9, 3, 7}))
+	fmt.Println(MaxNonAdjacentSum([]int{5, 1, 1, 5}, 0, make(map[int]int)))
 }
